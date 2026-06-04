b3753538d95e220950c4fc15edabc7de130689e1
Ayon Banerjee <ayon6237@gmail.com>
2026-06-04 12:24:06 +0600

    new

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -16,14 +16,7 @@ const Projects = () => {
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/ayon6237/CRUD_app_react_reduxToolkit">GitHub</a>
       </div>
     </div>
-    <div className="project-card">
-      <h3>CRUD application using React & React-Redux</h3>
-      <p>React + Redux based CRUD app</p>
-      <div className="project-links">
-        <a target="_blank" rel="noopener noreferrer" href="https://ayon6237.github.io/CRUD_app_react_reduxToolkit/">Live</a>
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/ayon6237/CRUD_app_react_reduxToolkit">GitHub</a>
-      </div>
-    </div>
+    
  
 
   </div>
